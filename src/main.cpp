f6e5aa586dd33d77f5fc5d0b96643f74de43c450
atlas604 <blitzbeat@gmail.com>
2017-08-24 06:23:12 -0400

    Add files via upload

--- src/main.cpp
+++ src/main.cpp
@@ -28,12 +28,17 @@ std::string hasData(std::string s) {
   return "";
 }
 
-int main()
+int main() // int argc, char *argv[]
 {
   uWS::Hub h;
 
   PID pid;
   // TODO: Initialize the pid variable.
+
+  double init_Kp = -0.095;
+  double init_Ki = 0;
+  double init_Kd = -2.5;
+  pid.Init(init_Kp, init_Ki, init_Kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -57,13 +62,16 @@ int main()
           * NOTE: Feel free to play around with the throttle and speed. Maybe use
           * another PID controller to control the speed!
           */
-          
+
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
+
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
+          msgJson["throttle"] = 0.2;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
